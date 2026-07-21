469f84da1241dd31cd6b1fc3d4f41828f8979662
a-m-i-r-888 <299061652+a-m-i-r-888@users.noreply.github.com>
2026-07-21 14:03:12 +0330

    update form

--- Js/currentPage.js
+++ Js/currentPage.js
@@ -1,7 +1,5 @@
 const currentPage = window.location.pathname.split("/").pop();
-console.log(currentPage);
 document.querySelectorAll("header nav a").forEach(link => {
-  console.log(link.getAttribute('href') === currentPage)
   if (link.getAttribute("href") === currentPage) {
     link.classList.add("active");
   }
