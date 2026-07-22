bc36ecc0f44e75dbcd61335ebc341b6369d7fbc3
a-m-i-r-888 <299061652+a-m-i-r-888@users.noreply.github.com>
2026-07-22 14:39:47 +0330

    horizontal scrollbar added

---
+++ Js/horizontalScrollbar.js
@@ -0,0 +1,14 @@
+// horizontal scrollbar
+
+const progressBar = document.querySelector('.scroll-progress');
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.scrollY;
+
+  const documentHeight =
+    document.documentElement.scrollHeight - window.innerHeight;
+
+  const scrollPercent = (scrollTop / documentHeight) * 100;
+
+  progressBar.style.width = scrollPercent + '%';
+});
